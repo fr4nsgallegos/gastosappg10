556d430ff0a484428c06f32fabdec49243109c3a
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-30 11:20:40 -0500

    item gasto

--- lib/widgets/item_gasto_widget.dart
+++ lib/widgets/item_gasto_widget.dart
@@ -5,6 +5,39 @@ class ItemGastoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile();
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8),
+      decoration: BoxDecoration(
+        color: Colors.black.withOpacity(0.05),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: ListTile(
+        title: Text(
+          "Cine",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        subtitle: Text(
+          "2024-30-11",
+          style: TextStyle(
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        trailing: Text(
+          "S/ 50.0",
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        leading: Image.asset(
+          "assets/images/alimentos.webp",
+          width: 40,
+          height: 40,
+        ),
+      ),
+    );
   }
 }
