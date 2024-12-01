2066f1409461b4f4ee10f6ecaa9ef64964aabb03
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-12-01 09:39:07 -0500

    check database

--- lib/db/db_admin.dart
+++ lib/db/db_admin.dart
@@ -5,12 +5,22 @@ import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DbAdmin {
-  initDatabase() async {
+  Database? myDatabase;
+
+  checkDatabase() async {
+    if (myDatabase == null) {
+      myDatabase = await initDatabase();
+    } else {
+      return myDatabase;
+    }
+  }
+
+  Future<Database> initDatabase() async {
     Directory directory = await getApplicationDocumentsDirectory();
     print(directory);
     String pathDatabase = join(directory.path, "GastosDB.db");
     print(pathDatabase);
-    openDatabase(pathDatabase, version: 1,
+    return await openDatabase(pathDatabase, version: 1,
         onCreate: (Database db, int version) {
       db.execute("""CREATE TABLE GASTOS (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
